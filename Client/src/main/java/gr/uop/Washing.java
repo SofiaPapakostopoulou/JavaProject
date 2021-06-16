e32b75448e7c2f0bbe51159a56479b14507cc3e7
Aikaterini Maria Gerakianaki <katerinagerak99@gmail.com>
2021-06-16 11:03:47 +0300

    abstract method

---
+++ Client/src/main/java/gr/uop/Washing.java
@@ -0,0 +1,45 @@
+package gr.uop;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public abstract class Washing {
+    /*
+     * private LocalDate date; private LocalTime time; private String uniqueID;
+     */
+    private String plate;
+    private double cost;
+    private Map<String, Double> servises = new HashMap<String, Double>();
+
+    public Washing(String plate, double cost) {
+        /*
+         * uniqueID = UUID.randomUUID().toString(); date = LocalDate.now(); time =
+         * LocalTime.now();
+         */
+        this.plate = plate;
+        this.cost = cost;
+    }
+
+    /*
+     * public LocalDate getLocalDate(){ return date; }
+     * 
+     * public LocalTime getLocalTime(){ return time; }
+     * 
+     * public String getID(){ return uniqueID; }
+     */
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public Map getServices() {
+        return servises;
+    }
+}
